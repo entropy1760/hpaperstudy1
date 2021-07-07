380e8ec5150c7e37bfdaaf155b41f6d0e6ca4765
manofbackend <entropy1760@gmail.com>
2021-07-07 12:55:20 +0900

    ~p223

---
+++ src/main/java/com/hpaper/book/springboot/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package com.hpaper.book.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+
+}
